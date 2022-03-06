3322e2a30e5bd0fb60df7bf2a368d33f40e12119
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-06 15:40:54 +0800

    Connect the public transport patches

---
+++ Source/PublicTransportUnstucker/Patch_TrolleybusAI_AntiRogue.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PublicTransportUnstucker
+{
+    [HarmonyPatch(typeof(TrolleybusAI))]
+    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    public class Patch_TrolleybusAI_AntiRogue
+    {
+        [HarmonyPrefix]
+        public static void PreFix(ushort vehicleID, ref Vehicle vehicleData)
+        {
+            if (vehicleData.m_waitCounter > 0 && vehicleData.m_waitCounter % 4 == 0)
+            {
+                // do not check too often, go easy on the CPU
+                RoguePassengerTable.FixInvalidPublicTransitPassengers(vehicleID, ref vehicleData);
+            }
+        }
+    }
+}
