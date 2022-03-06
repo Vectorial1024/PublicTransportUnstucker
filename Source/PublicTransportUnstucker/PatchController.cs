2da37365abf564043a3ad5e7a7bd5fb7c27effaa
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-06 14:39:56 +0800

    Code scaffolding

---
+++ Source/PublicTransportUnstucker/PatchController.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace PublicTransportUnstucker
+{
+    internal class PatchController
+    {
+        public static string HarmonyModID
+        {
+            get
+            {
+                return "com.vectorial1024.cities.ptu";
+            }
+        }
+
+        /*
+         * The "singleton" design is pretty straight-forward.
+         */
+
+        private static Harmony harmony;
+
+        public static Harmony GetHarmonyInstance()
+        {
+            if (harmony == null)
+            {
+                harmony = new Harmony(HarmonyModID);
+            }
+
+            return harmony;
+        }
+
+        public static void Activate()
+        {
+            GetHarmonyInstance().PatchAll(Assembly.GetExecutingAssembly());
+            //BusPickDropLookupTable.EnsureTableExists();
+            //CitizenRunawayTable.EnsureTableExists();
+        }
+
+        public static void Deactivate()
+        {
+            GetHarmonyInstance().UnpatchAll(HarmonyModID);
+            //BusPickDropLookupTable.WipeTable();
+            //CitizenRunawayTable.WipeTable();
+        }
+    }
+}
--- Source/PublicTransportUnstucker/PublicTransportUnstucker.cs
+++ Source/PublicTransportUnstucker/PublicTransportUnstucker.cs
@@ -1,11 +1,78 @@
-﻿using System;
+﻿using CitiesHarmony.API;
+using ICities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace PublicTransportUnstucker
 {
-    public class PublicTransportUnstucker
+    public class PublicTransportUnstucker : LoadingExtensionBase, IUserMod
     {
+        public virtual string Name
+        {
+            get
+            {
+                return "Public Transport Unstucker";
+            }
+        }
+
+        public virtual string Description
+        {
+            get
+            {
+                return "Fixes stuck public transport by reacting to known causes of getting stuck.";
+            }
+        }
+
+        /// <summary>
+        /// Executed whenever a level completes its loading process.
+        /// This mod the activates and patches the game using Hramony library.
+        /// </summary>
+        /// <param name="mode">The loading mode.</param>
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            /*
+             * This function can still be called when loading up the asset editor,
+             * so we have to check where we are right now.
+             */
+
+            switch (mode)
+            {
+                case LoadMode.LoadGame:
+                case LoadMode.NewGame:
+                case LoadMode.LoadScenario:
+                case LoadMode.NewGameFromScenario:
+                    break;
+
+                default:
+                    return;
+            }
+
+            UnifyHarmonyVersions();
+            PatchController.Activate();
+        }
+
+        /// <summary>
+        /// Executed whenever a map is being unloaded.
+        /// This mod then undoes the changes using the Harmony library.
+        /// </summary>
+        public override void OnLevelUnloading()
+        {
+            UnifyHarmonyVersions();
+            PatchController.Deactivate();
+        }
+
+        private void UnifyHarmonyVersions()
+        {
+            if (HarmonyHelper.IsHarmonyInstalled)
+            {
+                // this code will redirect our Harmony 2.x version to the authoritative version stipulated by CitiesHarmony
+                // I will make it such that the game will throw hard error if Harmony is not found,
+                // as per my usual software deployment style
+                // the user will have to subscribe to Harmony by themselves. I am not their parent anyways.
+                // so this block will have to be empty.
+            }
+        }
     }
 }
