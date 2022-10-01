c7aa611f373a9a41a2454723bb95a01ac1d7ddf1
Vincent Wong <vincentwwy168@gmail.com>
2022-10-01 23:45:10 +0800

    Also include planes and ships for certainty
    Not sure if it will help, but just in case, we deploy it first.

---
+++ Source/PublicTransportUnstucker/Patch_PassengerPlaneAI_AntiRogue.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace PublicTransportUnstucker
+{
+    [HarmonyPatch(typeof(PassengerPlaneAI))]
+    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    public class Patch_PassengerPlaneAI_AntiRogue
+    {
+        [HarmonyPrefix]
+        public static void PreFix(ushort vehicleID, ref Vehicle vehicleData)
+        {
+            if (vehicleData.m_waitCounter > 0 && vehicleData.m_waitCounter % 20 == 0)
+            {
+                // do not check too often, go easy on the CPU
+                RoguePassengerTable.FixInvalidPublicTransitPassengers(vehicleID, ref vehicleData);
+            }
+        }
+    }
+}
---
+++ Source/PublicTransportUnstucker/Patch_PassengerShipAI_AntiRogue.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+
+namespace PublicTransportUnstucker
+{
+    [HarmonyPatch(typeof(PassengerShipAI))]
+    [HarmonyPatch("CanLeave", MethodType.Normal)]
+    public class Patch_PassengerShipAI_AntiRogue
+    {
+        [HarmonyPrefix]
+        public static void PreFix(ushort vehicleID, ref Vehicle vehicleData)
+        {
+            if (vehicleData.m_waitCounter > 0 && vehicleData.m_waitCounter % 4 == 0)
+            {
+                // do not check too often, go easy on the CPU
+                RoguePassengerTable.FixInvalidPublicTransitPassengers(vehicleID, ref vehicleData);
+            }
+        }
+    }
+}
